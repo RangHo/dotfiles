0f94cdd28f480e8e2fcc592a19ac39a1cb618856
RangHo Lee <hello@rangho.me>
2021-01-26 15:41:38 +0900

    [wip] Improve circular dependency detection mechanism haha literate programming goes brrr

--- _functions/checks.sh
+++ _functions/checks.sh
@@ -12,26 +12,79 @@ ensure() {
 
 ## Maybe a command exists
 maybe() {
-    is_command $1 && "$@"
+    is $1 a command?
+    yes && "$@" || debug "No such command: $1"
 }
 
-## Check if specified command exists
-is_command() {
-    command -v "$1" > /dev/null
+## is-checks
+is() {
+    # Syntax: is <subject> a/an <complement>?
+
+    local subject article complement
+    subject="$1"
+    article="$2"
+    complement=$(echo "$3" | sed -e 's/\?$//')
+
+    if [ "$#" -ne 3 ] || [ "$article" != "a" ] && [ "$article" != "an" ]; then
+        error "Usage: is <subject> a(n) <complement>?"
+        return 1
+    fi
+
+    case "$complement" in
+        command) command -v "$subject" > /dev/null ;;
+        file) [ -e "$subject" ] ;;
+        directory) [ -d "$subject" ] ;;
+        symlink) [ -L "$subject" ] ;;
+
+        *)
+            error "<complement> can be either command, file, directory, or symlink."
+            false
+            ;;
+    esac && checks_last_result="yes" || checks_last_result="no"
 }
 
-## Check if specified file exists
-is_file() {
-    [ -e "$1" ]
+## does-checks
+does() {
+    # Syntax: does <subject> <verb> <object>?
+
+    local subject verb object
+    subject="$1"
+    verb="$2"
+    object=$(echo "$3" | sed -e 's/\?$//')
+
+    if [ "$#" -ne 3 ]; then
+        error "Usage: does <subject> <verb> <object>?"
+        return 1
+    fi
+
+    case "$verb" in
+        contain) does_contain "$subject" "$object" ;;
+
+        *)
+            error "<verb> must be contain."
+            false
+            ;;
+    esac && checks_last_result="yes" || checks_last_result="no"
+
 }
 
-## Check if specified directory exists
-is_directory() {
-    [ -d "$1" ]
+## Check if the "array" contains the item
+does_contain() {
+    local array item occurrence
+    array=$1
+    item=$2
+    occurrence=$(printf "%s\n" $(eval "echo \"\$$array\"") | grep -c "^$item$")
+
+    [ "$occurrence" -ne 0 ]
 }
 
-## Check if specified symbolic link exists
-is_symlink() {
-    [ -L "$1" ]
+## Yes check
+yes() {
+    [ "$checks_last_result" = "yes" ]
+}
+
+## No check
+no() {
+    [ "$checks_last_result" != "yes" ]
 }
 
--- _functions/logging.sh
+++ _functions/logging.sh
@@ -1,6 +1,24 @@
 # shellcheck shell=ash
 # This script assumes at least `local` extension is enabled.
 
+# Color constants
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+
+# Text style constants
+BOLD=$(tput bold)
+UNDERLINE=$(tput smul)
+BLINK=$(tput blink)
+
+# Reset test to normal style
+NORMAL=$(tput sgr0)
+
 debug() {
     [ "$DOTFILE_DEBUG" -eq 0 ] && [ "$DOTFILE_LOG_LEVEL" -lt 3 ] && return
     printf "[${BOLD}${WHITE}DEBUG${NORMAL}] ${WHITE}%s${NORMAL}\n" "$@" >&2

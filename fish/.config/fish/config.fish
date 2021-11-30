2055f87c8f5d0c92b8b121ad530247ce1a96bc22
RangHo Lee <hello@rangho.me>
2021-11-30 17:17:32 +0900

    [fish] Apply WSL-specific changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,27 +1,46 @@
 #!/bin/env fish
 
 # Bootstrap Fisher package manager if not installed already
-if ! type -q fisher; and test "$__fish_fisher_installed" != yes
+if not type -q fisher
     # Download fisher
     echo "Fisher package manager is not installed."
     echo "Downloading Fisher package manager..."
     curl -sL https://git.io/fisher | source
     echo "Finished downloading Fisher!"
 
-    # Set universal variable lest fish recurse infinitely
-    set --universal __fish_fisher_installed yes
+    # Change XDG_CONFIG_HOME temporarily to prevent nonexistent commmands
+    # from being executed
+    set tmpdir (mktemp --directory)
+    set xdg_config_home_backup $XDG_CONFIG_HOME
+    set -x XDG_CONFIG_HOME $tmpdir
 
     # Install fisher properly
     #fisher install jorgebucaran/fisher
 
     # Install other plugins
     fisher update
+
+    # Delete the temporary directory and restore XDG_CONFIG_HOME
+    rm -rf $tmpdir
+    set -gx XDG_CONFIG_HOME $xdg_config_home_backup
+end
+
+# If fish_plugins file is different, run fisher update
+diff -q -w (cat $__fish_config_dir/fish_plugins | sort | psub) (fisher list | sort | psub); \
+    or fisher update
+
+# Fix curses-based GPG pinentry screen
+set -gx GPG_TTY (tty)
+
+# If on WSL, then set VcXsrv as main display
+if uname -r | grep -q -e [Mm]icrosoft
+    set -gx DISPLAY (grep nameserver /etc/resolv.conf | cut -d ' ' -f 2):0
+    set -gx LIBGL_ALWAYS_INDIRECT 1
 end
 
 # Import wal colorscheme, if exists
-if test -e ~/.cache/wal/sequences
-    /bin/cat ~/.cache/wal/sequences
-end
+test -e ~/.cache/wal/sequences; \
+    and /bin/cat ~/.cache/wal/sequences
 
 # If there is ~/.profile, then source the file
 if test -e ~/.profile

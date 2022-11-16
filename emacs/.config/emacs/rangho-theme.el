bd6e669b3d2ac5eb678eea25be7db641c3dbfb50
RangHo Lee <hello@rangho.me>
2022-11-17 00:36:58 +0900

    [emacs] Implement proper font handling Emacs has its own funky way of dealing with fonts. Check out the article explaining Emacs internals: https://archive.casouri.cat/note/2021/fontset/index.html

--- emacs/.config/emacs/rangho-theme.el
+++ emacs/.config/emacs/rangho-theme.el
@@ -11,18 +11,32 @@
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 
-;; Set default font and frame style
-(set-face-font 'default "VictorMono Nerd Font-12")
-(set-fontset-font t 'hangul "Noto Sans Mono CJK KR-12")
-(set-fontset-font t 'kana "Noto Sans Mono CJK JP-12")
-(set-fontset-font t 'han "Noto Sans Mono CJK CN-12")
+;; Font-related configurations
+;; Emacs has weird way of implementing font lookup, so the function calls below cannot be
+;; tampered, or things *will* break.
+;;
+;; Sample sentences are provided below for reference:
+;;   - Alphabet : The quick brown fox jumps over the lazy dog.
+;;   - Hangul   : 다람쥐 헌 쳇바퀴에 타고파.
+;;   - Kana     : いろはにほへと ちりぬるを / わかよたれそ つねならむ
+;;   - Emoji(?) : I'm blue, da💨🎟️ ba🐋🐟 dee 🐦👖da📘🔷 ba💙💠 dai🌊🌀
+(set-face-attribute 'default nil
+                    :font "semteulche"
+                    :height 110)
+(set-fontset-font t 'hangul (font-spec :name "Noto Sans Mono CJK KR"))
+(set-fontset-font t 'kana (font-spec :name "Noto Sans Mono CJK JP"))
+(set-fontset-font t 'han (font-spec :name "Noto Sans Mono CJK CN"))
+(set-fontset-font t 'unicode (font-spec :name "semteulche"))
+(set-fontset-font t
+                  (if (version< emacs-version "28.1")
+                      '(#x1f300 . #x1fad0)
+                    'emoji)
+                  (font-spec :name "Noto Color Emoji"))
+
+;; Set default frame style
 (setq default-frame-alist
-      (append (list '(width . 72)
-                    '(height . 40)
-                    '(internal-border-width . 24)
-                    '(font . "VictorMono Nerd Font-12"))))
-(set-frame-parameter (selected-frame)
-                     'internal-border-width 24)
+      (append (list '(width . 80)
+                    '(height . 25))))
 
 ;; No ugly checkboxes
 (setq widget-image-enable nil)

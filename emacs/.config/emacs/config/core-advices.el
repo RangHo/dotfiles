103099147352c5699e789440648d4a3bb8661579
RangHo Lee <hello@rangho.me>
2022-12-14 01:28:22 +0900

    [emacs] Use more modern version of function advice

--- emacs/.config/emacs/config/core-advices.el
+++ emacs/.config/emacs/config/core-advices.el
@@ -1,19 +1,15 @@
 ;;; core-advices.el --- List of useful advices that make my life easier
 
 ;; Delete a line without worrying about excess spaces.
-(defadvice kill-line (before kill-line-autoreindent activate)
+(defun rangho/kill-excess-whitespaces (_)
   "Kill excess whitespace when joining lines.
 If the next line is joined to the current line, kill the extra indent whitespace in front of the next line."
   (when (and (eolp) (not (bolp)))
     (save-excursion
       (forward-char 1)
       (just-one-space 1))))
-(defadvice kill-visual-line (before kill-line-autoreindent activate)
-  "Kill excess whitespace when joining lines.
-If the next line is joined to the current line, kill the extra indent whitespace in front of the next line."
-  (when (and (eolp) (not (bolp)))
-    (save-excursion
-      (forward-char 1)
-      (just-one-space 1))))
+
+(advice-add 'kill-line :before #'rangho/kill-excess-whitespaces)
+(advice-add 'kill-visual-line :before #'rangho/kill-excess-whitespaces)
 
 (provide 'core-advices)

cd2c2fda144a226bcb419a20e3df0bea67ed45e3
RangHo Lee <hello@rangho.me>
2023-02-03 19:40:16 +0900

    [emacs] Add GitHub Copilot plugin

---
+++ emacs/.config/emacs/config/utility-copilot.el
@@ -0,0 +1,18 @@
+;;; utility-copilot.el --- GitHub Copilot support for Emacs
+
+;; Define custom <tab> behavior
+(defun rangho/copilot-tab ()
+  (interactive)
+  (or (copilot-accept-completion)
+      (indent-for-tab-command)))
+
+;; Install GitHub Copilot unofficial plugin
+(use-package copilot
+  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :after evil
+  :ensure t
+  :hook (prog-mode . copilot-mode)
+  :init (evil-define-key 'insert copilot-mode-map (kbd "<tab>") #'rangho/copilot-tab))
+
+
+(provide 'utility-copilot)

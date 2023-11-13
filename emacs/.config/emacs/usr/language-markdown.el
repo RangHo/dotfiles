2147926a5bad8e569f221c47843fde131c7f409d
RangHo Lee <hello@rangho.me>
2023-11-13 22:57:58 +0100

    [emacs] Always show 80-column indicator for Markdown files

--- emacs/.config/emacs/usr/language-markdown.el
+++ emacs/.config/emacs/usr/language-markdown.el
@@ -12,7 +12,158 @@
   <head>
     <meta charset='utf-8' />
     <script src='https://cdn.jsdelivr.net/npm/marked/marked.min.js'></script>
-    <style type='text/css'>@import url(http://fonts.googleapis.com/css?family=Vollkorn:400,400italic,700,700italic&subset=latin);h1,p{margin-top:0}ol,ul{padding-left:1.2em}body,h1 a,h1 a:hover{color:#333}a,h1 a,h1 a:hover{text-decoration:none}hr,video{margin:2em 0}img{max-width:100%%}table{width:100%%}table,td,th{padding:5px}body,html{padding:1em;margin:auto;max-width:72em;background:#fefefe}body{font:1.3em/1.4 Vollkorn,Palatino,Times;text-align:justify}h1,h2,h3{font-weight:400}h3,nav{font-style:italic}code,nav{font-size:.9em}article,footer,header,nav{width:700px;margin:0 auto}article{margin-top:4em;margin-bottom:4em;min-height:400px}footer{margin-bottom:50px}video{border:1px solid #ddd}nav{border-bottom:1px solid #ddd;padding:1em 0}nav p{margin:0}h3{margin-top:3em}p{-webkit-hypens:auto;-moz-hypens:auto;hyphens:auto}ul{list-style:square}blockquote{margin-left:1em;padding-left:1em;border-left:1px solid #ddd}code{font-family:Consolas,Menlo,Monaco,monospace,serif;background:#fff}a{color:#2484c1}a:hover{text-decoration:underline}a img{border:none}hr{color:#ddd;height:1px;border-top:1px solid #ddd;border-bottom:none;border-left:0;border-right:0}p#heart{font-size:2em;line-height:1;text-align:center;color:#ccc}.red{color:#b50000}body#index li{margin-bottom:1em}@media only screen and (max-device-width:1024px){body{font-size:120%%;line-height:1.4}}@media only screen and (max-device-width:480px){body{text-align:left}article,footer{width:auto}article{padding:0 10px}}</style>
+    <style type='text/css'>
+        @import url(http://fonts.googleapis.com/css?family=Vollkorn:400, 400italic, 700, 700italic&subset=latin);
+        h1,
+        p {
+            margin-top: 0;
+        }
+        ol,
+        ul {
+            padding-left: 1.2em;
+        }
+        body,
+        h1 a,
+        h1 a:hover {
+            color: #333;
+        }
+        a,
+        h1 a,
+        h1 a:hover {
+            text-decoration: none;
+        }
+        hr,
+        video {
+            margin: 2em 0;
+        }
+        img {
+            max-width: 100%%;
+        }
+        table {
+            width: 100%%;
+        }
+        table,
+        td,
+        th {
+            padding: 5px;
+        }
+        body,
+        html {
+            padding: 1em;
+            margin: auto;
+            max-width: 72em;
+            background: #fefefe;
+        }
+        body {
+            font: 1.3em/1.4 Vollkorn, Palatino, Times;
+            text-align: justify;
+        }
+        h1,
+        h2,
+        h3 {
+            font-weight: 400;
+        }
+        h3,
+        nav {
+            font-style: italic;
+        }
+        code,
+        nav {
+            font-size: 0.9em;
+        }
+        article,
+        footer,
+        header,
+        nav {
+            width: 700px;
+            margin: 0 auto;
+        }
+        article {
+            margin-top: 4em;
+            margin-bottom: 4em;
+            min-height: 400px;
+        }
+        footer {
+            margin-bottom: 50px;
+        }
+        video {
+            border: 1px solid #ddd;
+        }
+        nav {
+            border-bottom: 1px solid #ddd;
+            padding: 1em 0;
+        }
+        nav p {
+            margin: 0;
+        }
+        h3 {
+            margin-top: 3em;
+        }
+        p {
+            -webkit-hypens: auto;
+            -moz-hypens: auto;
+            hyphens: auto;
+        }
+        ul {
+            list-style: square;
+        }
+        blockquote {
+            margin-left: 1em;
+            padding-left: 1em;
+            border-left: 1px solid #ddd;
+        }
+        code {
+            font-family: Consolas, Menlo, Monaco, monospace, serif;
+            background: #fff;
+        }
+        a {
+            color: #2484c1;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+        a img {
+            border: none;
+        }
+        hr {
+            color: #ddd;
+            height: 1px;
+            border-top: 1px solid #ddd;
+            border-bottom: none;
+            border-left: 0;
+            border-right: 0;
+        }
+        p#heart {
+            font-size: 2em;
+            line-height: 1;
+            text-align: center;
+            color: #ccc;
+        }
+        .red {
+            color: #b50000;
+        }
+        body#index li {
+            margin-bottom: 1em;
+        }
+        @media only screen and (max-device-width: 1024px) {
+            body {
+                font-size: 120%%;
+                line-height: 1.4;
+            }
+        }
+        @media only screen and (max-device-width: 480px) {
+            body {
+                text-align: left;
+            }
+            article,
+            footer {
+                width: auto;
+            }
+            article {
+                padding: 0 10px;
+            }
+        }
+    </style>
     <title>Emacs Markdown Preview</title>
   </head>
   <body>
@@ -54,9 +205,13 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
+  :hook ((markdown-mode . (lambda ()
+                            (setq-local display-fill-column-indicator-column 80)
+                            (display-fill-column-indicator-mode 1))))
   :init
   (setq markdown-command "multimarkdown")
-  (setq markdown-fontify-code-blocks-natively t))
+  (setq markdown-fontify-code-blocks-natively t)
+  (setq-local))
 
 ;; Create helper command to start web server and impatient mode
 (defun rangho/markdown-preview ()

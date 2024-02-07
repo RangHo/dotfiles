b88e1a3b1c9d306c7d118439882c5cbd0c1e8170
RangHo Lee <hello@rangho.me>
2024-02-07 15:06:30 +0100

    fix(emacs): add pre-build step for AUCTeX

--- emacs/.config/emacs/usr/language-latex.el
+++ emacs/.config/emacs/usr/language-latex.el
@@ -8,6 +8,11 @@
 
 ;; Install AUCTeX
 (use-package auctex
+  :elpaca (auctex :pre-build (("./autogen.sh")
+                              ("./configure" "--without-texmf-dir" "--with-lispdir=.")
+                              ("make")
+                              ("install-info" "doc/auctex.info" "doc/dir")
+                              ("install-info" "doc/preview-latex.info" "doc/dir")))
   :hook (LaTeX-mode . prettify-symbols-mode))
 
 (provide 'language-latex)

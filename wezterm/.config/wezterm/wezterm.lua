628b2d00c0d8a59be8c0d7ceee1aa90519205de2
RangHo Lee <hello@rangho.me>
2024-03-05 17:02:56 +0900

    feat(wezterm): migrate from Kitty to WezTerm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm'
+
+return {
+   -- Window settings
+   window_background_opacity = 0.8,
+   window_padding = {
+      left = 15,
+      right = 15,
+      top = 15,
+      bottom = 15,
+   },
+   hide_tab_bar_if_only_one_tab = true,
+
+   -- Font-related
+   font = wezterm.font_with_fallback {
+      'semteulche',
+      'Noto Sans Mono',
+      'FontAwesome',
+   },
+   font_size = 14.0,
+}

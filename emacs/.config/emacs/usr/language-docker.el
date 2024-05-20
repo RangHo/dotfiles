b86ad4aff72e71954982f0ed52ecf4273169974f
RangHo Lee <hello@rangho.me>
2024-05-20 22:59:59 +0900

    fix(emacs): match parentheses

--- emacs/.config/emacs/usr/language-docker.el
+++ emacs/.config/emacs/usr/language-docker.el
@@ -9,7 +9,7 @@
 (use-package dockerfile-mode
   :mode (("Dockerfile\\'" . dockerfile-mode)
          ("Containerfile\\'" . dockerfile-mode)
-         ("\\.dockerfile\\'" . dockerfile-mode))
+         ("\\.dockerfile\\'" . dockerfile-mode)))
 
 (provide 'language-docker)
 

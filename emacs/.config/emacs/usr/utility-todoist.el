ad44efd02f9ad8e0e6e7f48471104ef9b2d156ca
RangHo Lee <hello@rangho.me>
2024-05-31 21:31:25 +0900

    feat(emacs): add Todoist integration

---
+++ emacs/.config/emacs/usr/utility-todoist.el
@@ -0,0 +1,14 @@
+;;; utility-todoist.el --- Todoist integration for Emacs
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(use-package todoist
+  :bind ("C-c A" . todoist))
+
+(provide 'utility-todoist)
+
+;;; utility-todoist.el ends here

4cc974c2fc99058ebb3e8725cf6af5afe76ed556
RangHo Lee <hello@rangho.me>
2026-04-22 15:36:35 +0900

    fix: don't quote grep pattern

--- dot_config/exact_profile.d/10-environment.sh
+++ dot_config/exact_profile.d/10-environment.sh
@@ -1,12 +1,10 @@
-if [ -d "$HOME/.config/environment.d" ]; then
-    for env_file in "$HOME/.config/environment.d/*.conf"; do
-        if [ -r "$env_file" ]; then
-            vars=$(grep -E '^[A-Za-z_][A-Za-z0-9_]*=' "$env_file")
-            . "$env_file"
-            for var in $vars; do
-                var_name=$(echo "$var" | cut -d= -f1)
-                export "$var_name"
-            done
-        fi
-    done
-fi
+for env_file in $HOME/.config/environment.d/*; do
+    if [ -r "$env_file" ]; then
+        vars=$(grep -E '^[A-Za-z_][A-Za-z0-9_]*=' "$env_file")
+        . "$env_file"
+        for var in $vars; do
+            var_name=$(echo "$var" | cut -d= -f1)
+            export "$var_name"
+        done
+    fi
+done

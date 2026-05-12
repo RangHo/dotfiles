071f531a502d74ff49f712e4b5a51954bae34aed
RangHo Lee <hello@rangho.me>
2026-05-12 16:48:50 +0900

    feat(emacs): remove theme directory as well

--- dot_config/emacs/early-init.el
+++ dot_config/emacs/early-init.el
@@ -50,7 +50,19 @@
 
   (defun no-littering-theme-custom-themes ()
     "Theme the Emacs custom theme location."
-    (setq custom-theme-directory (no-littering-expand-etc-file-name "themes/")))
+    (let ((old-custom-themes (expand-file-name "themes/" user-emacs-directory))
+          (new-custom-themes (no-littering-expand-etc-file-name "themes/")))
+      ;; Set the new custom themes directory.
+      (setq custom-theme-directory new-custom-themes)
+      ;; Move the contents of the old custom themes directory to the new custom themes directory.
+      (when (file-exists-p old-custom-themes)
+        (unless (file-exists-p new-custom-themes)
+          (make-directory new-custom-themes t))
+        (dolist (file (directory-files old-custom-themes t))
+          (when (file-regular-p file)
+            (copy-file file (expand-file-name (file-name-nondirectory file) new-custom-themes) t)))))
+    ;; Delete the old custom themes directory.
+    (delete-directory old-custom-themes t))
 
   (defun no-littering-theme-eln-cache ()
     "Theme the Emacs native compilation cache.
@@ -58,32 +70,32 @@
     This function will move the littered eln-cache directory to the no-littering directory."
     (let ((old-eln-cache (expand-file-name "eln-cache/" user-emacs-directory))
           (new-eln-cache (no-littering-expand-var-file-name "eln-cache/")))
-      ;; Set the new eln-cache directory
+      ;; Set the new eln-cache directory.
       (startup-redirect-eln-cache new-eln-cache)
-      ;; Move the contents of the old eln-cache directory to the new eln-cache directory
+      ;; Move the contents of the old eln-cache directory to the new eln-cache directory.
       (when (file-exists-p old-eln-cache)
         (unless (file-exists-p new-eln-cache)
           (make-directory new-eln-cache t))
         (dolist (file (directory-files old-eln-cache t))
           (when (file-regular-p file)
             (copy-file file (expand-file-name (file-name-nondirectory file) new-eln-cache) t))))
-      ;; Delete the old eln-cache directory
+      ;; Delete the old eln-cache directory.
       (delete-directory old-eln-cache t)))
 
   (defun no-littering-theme-elpa ()
     "Theme the Emacs package.el directory."
     (let ((old-elpa (expand-file-name "elpa/" user-emacs-directory))
           (new-elpa (no-littering-expand-var-file-name "elpa/")))
-      ;; Set the new elpa directory
+      ;; Set the new elpa directory.
       (setq package-user-dir new-elpa)
-      ;; Move the contents of the old elpa directory to the new elpa directory
+      ;; Move the contents of the old elpa directory to the new elpa directory.
       (when (file-exists-p old-elpa)
         (unless (file-exists-p new-elpa)
           (make-directory new-elpa t))
         (dolist (file (directory-files old-elpa t))
           (when (file-regular-p file)
             (copy-file file (expand-file-name (file-name-nondirectory file) new-elpa) t))))
-      ;; Delete the old elpa directory
+      ;; Delete the old elpa directory.
       (delete-directory old-elpa t)))
 
   (defun no-littering-theme-treesit ()
@@ -92,16 +104,16 @@
   This function will move the littered tree-sitter directory to the no-littering directory."
     (let ((old-tree-sitter (expand-file-name "tree-sitter/" user-emacs-directory))
           (new-tree-sitter (no-littering-expand-var-file-name "tree-sitter/")))
-      ;; Set the new tree-sitter directory
+      ;; Set the new tree-sitter directory.
       (setq treesit-extra-load-path (list new-tree-sitter))
-      ;; Move the contents of the old tree-sitter directory to the new tree-sitter directory
+      ;; Move the contents of the old tree-sitter directory to the new tree-sitter directory.
       (when (file-exists-p old-tree-sitter)
         (unless (file-exists-p new-tree-sitter)
           (make-directory new-tree-sitter t))
         (dolist (file (directory-files old-tree-sitter t))
           (when (file-regular-p file)
             (copy-file file (expand-file-name (file-name-nondirectory file) new-tree-sitter) t))))
-      ;; Delete the old tree-sitter directory
+      ;; Delete the old tree-sitter directory.
       (delete-directory old-tree-sitter t)))
 
   (no-littering-theme-backups)
